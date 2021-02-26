62b9d91b1d5a454a09b5615f9727d42b6e2f9460
TaeKyeong Kim <67273859+cokeworld@users.noreply.github.com>
2021-02-26 23:35:29 +0900

    chapter3: spring data jpa & test code

---
+++ src/test/java/com/cokeworld/book/springboot/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,50 @@
+package com.cokeworld.book.springboot.domain.posts;
+
+import org.assertj.core.api.Assertions;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.Assert.*;
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기() {
+
+        //given
+        String title = "게시글 제목";
+        String content = "게시글 본문";
+        String author = "cokeWorld@gmail.com";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author(author)
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+}
