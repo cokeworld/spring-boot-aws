7f3e4a0f3d8c4f6d6fab597c6b8461c46323dbf2
TaeKyeong Kim <67273859+cokeworld@users.noreply.github.com>
2021-03-02 17:52:58 +0900

    chapter3: 등록/수정/조회 API 만들기 test

--- src/main/java/com/cokeworld/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/cokeworld/book/springboot/service/posts/PostsService.java
@@ -1,6 +1,8 @@
 package com.cokeworld.book.springboot.service.posts;
 
+import com.cokeworld.book.springboot.domain.posts.Posts;
 import com.cokeworld.book.springboot.domain.posts.PostsRepository;
+import com.cokeworld.book.springboot.web.dto.PostsResponseDto;
 import com.cokeworld.book.springboot.web.dto.PostsSaveRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -17,4 +19,21 @@ public class PostsService {
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
     }
+
+    @Transactional
+    public long update(Long id, PostsSaveRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+
+        return new PostsResponseDto(entity);
+    }
+
 }
