f1292b2a011cf904c5f8e7b8ff7ceda87ba6a3e0
TaeKyeong Kim <67273859+cokeworld@users.noreply.github.com>
2021-03-07 16:39:32 +0900

    chapter4: 게시글 수정, 삭제

--- src/main/java/com/cokeworld/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/cokeworld/book/springboot/service/posts/PostsService.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
@@ -46,5 +47,10 @@ public class PostsService {
                 .collect(Collectors.toList());
     }
 
-
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+        postsRepository.delete(posts);
+    }
 }
--- src/main/java/com/cokeworld/book/springboot/web/IndexController.java
+++ src/main/java/com/cokeworld/book/springboot/web/IndexController.java
@@ -1,10 +1,13 @@
 package com.cokeworld.book.springboot.web;
 
 import com.cokeworld.book.springboot.service.posts.PostsService;
+import com.cokeworld.book.springboot.web.dto.PostsResponseDto;
+import com.cokeworld.book.springboot.web.dto.PostsSaveRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -21,5 +24,13 @@ public class IndexController {
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
     }
 }
